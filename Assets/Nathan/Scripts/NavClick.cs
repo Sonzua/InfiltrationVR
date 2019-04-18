af3af77a50eac03b242c191c39a1920d6f16dd72
Vitaki <nathan.mendes@hotmail.fr>
2019-04-18 09:36:34 +0200

    dossier Nathan, scene

---
+++ Assets/Nathan/Scripts/NavClick.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavClick : MonoBehaviour
+{
+    public float shootDistance = 10f;
+    public float shootRate = .5f;
+
+    public NavMeshAgent navMeshAgent;
+    private Ray shootRay;
+    private RaycastHit shootHit;
+    private bool walking;
+
+    // Use this for initialization
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (Physics.Raycast(ray, out hit, 100))
+
+            {
+                walking = true;
+                navMeshAgent.destination = hit.point;
+            }
+        }
+
+        if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+        {
+            if (!navMeshAgent.hasPath || Mathf.Abs(navMeshAgent.velocity.sqrMagnitude) < float.Epsilon)
+                walking = false;
+        }
+        else
+        {
+            walking = true;
+        }
+    }
+}
