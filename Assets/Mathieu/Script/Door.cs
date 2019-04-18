d54b08e01b40f1b9b88cecc4fbe435ac3a1453b6
Nercros <miniblond33@gmail.com>
2019-04-18 10:07:03 +0200

    Scène Porte+ clé+ VRTK Set up

---
+++ Assets/Mathieu/Script/Door.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    public Transform door;
+    
+
+    public bool opening = false;
+    public bool closing = false;
+    public float speed = 0;
+    public float maxOpenValue;
+    public float currentValue = 0f;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(opening == true )
+        {
+            OpenDoor();
+        }
+        if (closing == true)
+        {
+            CloseDoor();
+        }
+    }
+
+    
+    private void OnTriggerEnter(Collider Clé)
+    {
+        if (Clé.gameObject.CompareTag("Clé"))
+        {
+            opening = true;
+            closing = false;
+        }
+    }
+    
+
+    
+    void OpenDoor()
+    {
+        float movement = speed * Time.deltaTime;
+        currentValue += movement;
+        if (currentValue <= maxOpenValue)
+        {
+            door.position = new Vector3(door.position.x, door.position.y, door.position.z + movement);
+        }
+        else
+        {
+            opening = false;
+        }
+    }
+
+    void CloseDoor()
+    {
+        float movement = speed * Time.deltaTime;
+        currentValue -= movement;
+        if (currentValue >= 0)
+        {
+            door.position = new Vector3(door.position.x, door.position.y, door.position.z - movement);
+        }
+        else
+        {
+            closing = false;
+        }
+    }
+}
